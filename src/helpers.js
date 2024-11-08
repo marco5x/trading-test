72cb6700c0cf443874adaab855154d5502b4be3a
MarcosX <markito5_net@hotmail.com>
2024-11-07 21:05:51 -0300

    helpers solutions

--- src/helpers.js
+++ src/helpers.js
@@ -1,29 +1,33 @@
-export const apiKey ="87f590cd48b6df443edd153c0a112bc373171ef881cbdf699025942166561c40"
+export const apiKey ="c4a53e34a7192e5d2c28027443d5b4fbf9f10f3c9a9368db6ab483d97fd9ffab"
 
 export async function makeApiRequest(path) {
-    try {
-        const url = new URL(`https://min-api.cryptocompare.com/${path}`);
-        url.searchParams.append('api_key',apiKey)
-        const response = await fetch(url.toString());
-        return response.json();
-    } catch (error) {
-        throw new Error(`CryptoCompare request error: ${error.status}`);
-    }
+	try {
+		const url = new URL(`https://min-api.cryptocompare.com/${path}`);
+		url.searchParams.append('api_key',apiKey)
+		const response = await fetch(url.toString());
+		return response.json();
+	} catch (error) {
+		throw new Error(`CryptoCompare request error: ${error.status}`);
+	}
 }
 
 // Generates a symbol ID from a pair of the coins
 export function generateSymbol(exchange, fromSymbol, toSymbol) {
-    const short = `${fromSymbol}/${toSymbol}`;
-    return {
-        short,
-    };
+	const short = `${fromSymbol}/${toSymbol}`;
+	return {
+		short,
+		full: `${exchange}:${short}`,
+	};
 }
 
 // Returns all parts of the symbol
 export function parseFullSymbol(fullSymbol) {
-    const match = fullSymbol.match(/^(\w+):(\w+)\/(\w+)$/);
-    if (!match) {
-        return null;
-    }
-    return { exchange: match[1], fromSymbol: match[2], toSymbol: match[3] };
+	const match = fullSymbol.match(/^(\w+):(\w+)\/(\w+)$/);
+	if (!match) return null;
+
+	return {
+		exchange: match[1],
+		fromSymbol: match[2],
+		toSymbol: match[3],
+	};
 }
